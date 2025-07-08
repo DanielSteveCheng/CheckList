3ca72e18e9d7519abf00f6cedb547991f7b43c23
DanielSteveCheng <daniel.cheng@msn.com>
2025-07-07 22:19:54 -0400

    Adding files

---
+++ checklist/src/app/components/list3.jsx
@@ -0,0 +1,61 @@
+// components/InteractiveList.jsx
+
+'use client';
+
+import { useState } from 'react';
+
+export default function list3() {
+  const [items, setItems] = useState(['Warframe', 'Overwatch', 'Spend time with friends']);
+  const [input, setInput] = useState('');
+
+  const handleDelete = (indexToDelete) => {
+    setItems(items.filter((_, index) => index !== indexToDelete));
+  };
+
+  const handleAdd = () => {
+    if (input.trim() !== '') {
+      setItems([...items, input]);
+      setInput('');
+    }
+  };
+
+  return (
+    <div className=" min-h-[50vh] w-[25vw] mx-auto p-10 bg-green-200 rounded-xl shadow transition-all duration-300 ease-in-out hover:scale-105">
+      <h1 className="text-2xl text-black mb-4">To Do in Your Free Time</h1>
+      
+      <div className="p-4 bg-green-300 rounded mb-4 flex items-center w-auto justify-between hover:scale-105 transition-all duration-300 ease-in-out">
+        <div>
+          <form
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              placeholder="Add item..."
+              className=" text-black hover:text-[18px] transition-all duration-300 w-[10vw] ease-in-out focus:outline-hidden"
+            />
+        </div>
+            
+        <div>
+          <button onClick={handleAdd} className="px-4 py-2 bg-green-400 text-white rounded hover:bg-green-500 transition-all duration-300 ease-in-out hover:scale-105">
+              +
+          </button>
+        </div>
+      </div>
+
+      {items.length === 0 ? (
+        <p className="text-black text-[3vh]">Finished! You did it!</p>
+      ) : (
+        <ul className="space-y-2">
+          {items.map((item, index) => (
+            <li
+              key={index}
+              onClick={() => handleDelete(index) }
+              className="cursor-pointer text-black bg-green-100 hover:bg-green-50 hover:scale-105 text-[15px] px-4 py-[3vh] rounded transition-all duration-300 ease-in-out"
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
---
+++ checklist/src/app/page.tsx
@@ -0,0 +1,33 @@
+import List1 from './components/list1';
+import List2 from './components/list2';
+import List3 from './components/list3';
+
+
+export default function Home() {
+  return (
+    <div className="min-h-screen
+    bg-gradient-to-r from-green-950 from-0% via-green-900 via-50% to-green-950 to-100%">
+      <main className="flex flex-col gap-[32px] items-center sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <h1 className=" text-[8vh]">✨Checklist✨</h1>
+        <p className="text-[2vh]">Hey Daniel! Here's your daily checklist. Remember, you got this!</p>
+      </main>
+      <div className=" mt-[-20vh] grid grid-cols-3 min-h-screen p-8 gap-[1vw] sm:p-20 font-[family-name:var(--font-geist-sans)] position-absolute items-center justify-items-center">
+        
+        <div className="animate-all animate-(--animate-slide1)">
+            <List2/>
+        </div>
+
+        <div className=" animate-all md:animate-(--animate-slide2)">
+            <List1/>
+        </div>
+        
+        <div className=" animate-all md:animate-(--animate-slide3)">
+            <List3/>
+        </div>
+
+      </div>
+    </div>
+
+
+  );
+}
